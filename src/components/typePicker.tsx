5c69c236c50b9ad49d11de08086735d9ef240188
Scott Key <scottdkey@gmail.com>
2021-05-02 17:08:24 -0600

    toggle state to open selector

--- src/components/typePicker.tsx
+++ src/components/typePicker.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react"
+import React, { FC, ReactChildren, useState } from "react"
 import { PokeTypes, PokemonTypesInterface, PokeType } from "./PokemonType"
 
 import "./typePicker.scss"
@@ -6,15 +6,18 @@ import "./typePicker.scss"
 
 
 const TypePicker: FC = () => {
-  const [selected, setSelected] = useState<string>("dark")
+  const [selected, setSelected] = useState<string>("dark");
+  const [openSelector, setOpenSelector] = useState<boolean>(false)
 
   const setState = (type: string) => {
     setSelected(type)
+    setOpenSelector(false)
   }
   const RenderAllTypes = ({ list }: { list: Array<PokemonTypesInterface> }) => {
-    return <div className="circle-container">{list.map((item: PokemonTypesInterface, index) => (
+    return <div className={`circle-container`}>{list.map((item: PokemonTypesInterface, index) => (
       <item.icon className={`icon ${item.type} ${index}`} key={item.type} onClick={() => setState(item.type)} />
     ))}</div>
+
   }
 
   type RenderSelectedType = {
@@ -34,7 +37,7 @@ const TypePicker: FC = () => {
     } else {
       return (
         <>
-          <p.icon className={`icon ${p.type} selected`} />
+          <p.icon className={`icon ${p.type} selected`} onClick={() => setOpenSelector(!openSelector)} />
         </>
       )
     }
@@ -44,7 +47,7 @@ const TypePicker: FC = () => {
   return (
     <div className="picker-container">
       <RenderSelected type={selected} />
-      <RenderAllTypes list={PokeTypes} />
+      {openSelector ? <RenderAllTypes list={PokeTypes} /> : null}
     </div >
   )
 }
